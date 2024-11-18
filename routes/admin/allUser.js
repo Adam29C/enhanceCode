a0199f37b771d42e12c96e4b0cf5f2ed9bb47bca
aayush22200 <168806480+aayush22200@users.noreply.github.com>
2024-11-18 13:57:42 +0530

    add the code

--- routes/admin/allUser.js
+++ routes/admin/allUser.js
@@ -154,7 +154,7 @@ router.get("/getProfile", authMiddleware, async (req, res) => {
         if (!userProfile) {
             return res.status(404).json({
                 status: false,
-                message: `Profile not found for user with id ${id}.`
+                message: `Profile Not Filled By User.`
             });
         }
 
