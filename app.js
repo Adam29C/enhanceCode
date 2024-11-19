a456c9ab193a21196119ada8b732b3b7854d7503
aayush22200 <168806480+aayush22200@users.noreply.github.com>
2024-11-19 12:39:20 +0530

    add code

--- app.js
+++ app.js
@@ -97,6 +97,7 @@ const abGameProvider=require("./routes/admin/andarBahar/abGameProvider")
 const abGameSetting =require("./routes/admin/andarBahar/abGameSetting")
 const abGameRate =require("./routes/admin/andarBahar/abGameRate.js")
 const abProfitLoss =require("./routes/admin/andarBahar/abProfitLoss1.js")
+const abGameResult =require("./routes/admin/andarBahar/abGameResult.js")
 const mainGameResult = require("./routes/admin/games/mainGameResult.js")
 const starLine = require("./routes/admin/starLine/starlineProvider");
 const starLineSetting = require("./routes/admin/starLine/starlineSetting");
